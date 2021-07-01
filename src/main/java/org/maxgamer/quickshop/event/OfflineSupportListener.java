6b0bd2aa66d6ef2f3047ecbeff49683e48a6bdb5
Mr_Coco <hihicoco0325@gmail.com>
2021-07-01 18:15:16 +0800

    Create OfflineSupportListener.java

---
+++ src/main/java/org/maxgamer/quickshop/event/OfflineSupportListener.java
@@ -0,0 +1,13 @@
+package org.maxgamer.quickshop.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class OfflineSupportListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+
+    }
+}
